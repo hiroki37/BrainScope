1529a3ef3b861ad0b10caa9e56ac6abf0e337f7e
HaineTakano <clann.libertybell@gmail.com>
2016-07-15 12:21:47 +0900

    誤ったプルの微修正

--- src/main/java/jp/co/sbps/NeuronDao.java
+++ src/main/java/jp/co/sbps/NeuronDao.java
@@ -144,7 +144,7 @@ public class NeuronDao {
 	public Float greatest(Float var1, Float var2) {
 		return jdbc.queryForObject("SELECT GREATEST(?, ?)", Float.class, var1, var2);
 	}
-	
+
 	// right_edge以上で最少のleft_edge
 	public Float min_left_edge(Float right_edge) {
 		return jdbc.queryForObject("SELECT MIN(left_edge) FROM neuron WHERE left_edge > ?", Float.class, right_edge);
