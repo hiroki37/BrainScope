b197784259177bb1e56c621c6bc8585c95ac07b1
Haine Takano <clann.libertybell@gmail.com>
2016-07-20 02:00:51 +0900

    １．SQLファイルの整理　２．masterブランチへの回帰

--- SQL_backup/schema.sql
+++ SQL_backup/schema.sql
@@ -1,9 +1,18 @@
+
+
 /* Create Tables */
 
-CREATE TABLE neuron
+CREATE TABLE config
+(
+	present_id int NOT NULL,
+	scope_size int NOT NULL
+) WITHOUT OIDS;
+
+
+CREATE TABLE neuron_pro
 (
 	id serial NOT NULL,
-	title varchar NOT NULL,
+	title varchar,
 	content text,
 	neuron_level int NOT NULL,
 	create_date date NOT NULL,
@@ -11,8 +20,9 @@ CREATE TABLE neuron
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE config
+
+CREATE TABLE tree_diagram
 (
-	scope_address int NOT NULL,
-	scope_size int NOT NULL
+	ancestor int NOT NULL,
+	descendant int NOT NULL
 );
